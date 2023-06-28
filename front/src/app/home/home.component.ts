39ec120861a3b0af8183a25941650b15be541fc9
Franck <41587017+Freddyede@users.noreply.github.com>
2023-06-28 13:18:32 +0200

    Updating projects

---
+++ front/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HttpClientService } from "../services/httpClient.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent {
+    constructor(private readonly httpServices: HttpClientService) { }
+}
